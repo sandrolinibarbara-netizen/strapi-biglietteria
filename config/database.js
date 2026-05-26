a0560d3873e8e5e1a9f93462943a36da1dee4fd3
barba <sandrolini.barbara@nebbialab.it>
2026-05-26 14:35:19 +0200

    SSL settings added

--- config/database.js
+++ config/database.js
@@ -3,13 +3,13 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         connection: {
             connectionString: env('DATABASE_URL'),
-            ssl: env.bool('DATABASE_SSL', false) && {
+            ssl: env.bool('DATABASE_SSL', true) && {
               key: env('DATABASE_SSL_KEY', undefined),
               cert: env('DATABASE_SSL_CERT', undefined),
               ca: env('DATABASE_SSL_CA', undefined),
               capath: env('DATABASE_SSL_CAPATH', undefined),
               cipher: env('DATABASE_SSL_CIPHER', undefined),
-              rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+              rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
             },
             schema: env('DATABASE_SCHEMA', 'public'),
           },
