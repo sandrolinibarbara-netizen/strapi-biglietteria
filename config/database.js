5d0779a226052058c4d089b5f21fa0098436bfa5
barba <sandrolini.barbara@nebbialab.it>
2026-05-26 14:25:28 +0200

    SSl settings added

--- config/database.js
+++ config/database.js
@@ -3,16 +3,16 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         connection: {
             connectionString: env('DATABASE_URL'),
-          ssl: env.bool('DATABASE_SSL', false) && {
-            key: env('DATABASE_SSL_KEY', undefined),
-            cert: env('DATABASE_SSL_CERT', undefined),
-            ca: env('DATABASE_SSL_CA', undefined),
-            capath: env('DATABASE_SSL_CAPATH', undefined),
-            cipher: env('DATABASE_SSL_CIPHER', undefined),
-            rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+            ssl: env.bool('DATABASE_SSL', false) && {
+              key: env('DATABASE_SSL_KEY', undefined),
+              cert: env('DATABASE_SSL_CERT', undefined),
+              ca: env('DATABASE_SSL_CA', undefined),
+              capath: env('DATABASE_SSL_CAPATH', undefined),
+              cipher: env('DATABASE_SSL_CIPHER', undefined),
+              rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+            },
+            schema: env('DATABASE_SCHEMA', 'public'),
           },
-          schema: env('DATABASE_SCHEMA', 'public'),
-        },
         debug: true,
         pool: { min: 0, max: 7 },
     }
